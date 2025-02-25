202ef85d06e0b4043c222bc8927f2d3d3b3d012f
Vincent Franco <vince@freshivore.net>
2025-02-24 23:09:34 -0600

    Fixup

--- keyboards/gggw/crosses/config.h
+++ keyboards/gggw/crosses/config.h
@@ -1,19 +1,3 @@
-/*
-Copyright 2024 Vincent Franco <me@vincentfranco.com>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 #pragma once
 
 #define SERIAL_PIO_USE_PIO1
@@ -65,13 +49,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SCROLL_DIVISOR_V 30.0
 
 /*
- * OLED
- */
-#define I2C_DRIVER I2CD1
-#define I2C1_SCL_PIN GP15
-#define I2C1_SDA_PIN GP14
-
-/*
  * Reset
  */
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
@@ -85,3 +62,4 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
  * Font
  */
 #define OLED_FONT_H "lib/crosses-font.c"
+
--- keyboards/gggw/crosses/rules.mk
+++ keyboards/gggw/crosses/rules.mk
@@ -1,5 +1,4 @@
 POINTING_DEVICE_ENABLE = yes
-POINTING_DEVICE_DRIVER = pmw3360
 OLED_ENABLE = yes
 
 SRC += lib/crosses.c
